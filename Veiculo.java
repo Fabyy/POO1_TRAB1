ec58be2001ff10548fc3f9f2cd9f57c19966ccd1
Fabyy <fabianne.palmejani@gmail.com>
2015-06-08 23:30:12 -0300

    versao2.0
    Fabianne

--- Veiculo.java
+++ Veiculo.java
@@ -2,42 +2,17 @@
 
 public abstract class Veiculo {
 	
-	protected int placa;		//identificador do veiculo
+	protected int ident;		//identificador do veiculo
 	protected int veloc_max; 	//limite de velocidade de cada tipo de veiculo
 	protected int pos; 			//posicao que o carro se encontra na via
+	protected int ind_via;		//indice da via na qual o veic está percorrendo
+	protected String nome;
 	
-	protected Veiculo(){
+
+		
+	public String getNome(){
+		return this.nome;
 	}
-	
-	public int getPlaca() {
-		return placa;
-	}
-
-
-	public int getVeloc_max() {
-		return veloc_max;
-	}
-
-
-	public int getPos() {
-		return pos;
-	}
-
-
-	public void setPlaca(int placa) {
-		this.placa = placa;
-	}
-
-
-	public void setVeloc_max(int veloc_max) {
-		this.veloc_max = veloc_max;
-	}
-
-
-	public void setPos(int pos) {
-		this.pos = pos;
-	} 
-	
 	
 	protected void anda(){ 		//gera o movimento do veiculo em cada hora
 		
